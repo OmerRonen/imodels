4c9c4b51a22745fd7ae6f39ca1060e20240a8702
Abhineet <aa3797@berkeley.edu>
2023-06-08 10:04:19 -0700

    adding dynamic logistic regression code

--- imodels/linear_models/d_logistic_regression.py
+++ imodels/linear_models/d_logistic_regression.py
@@ -17,6 +17,8 @@ from scipy.special import expit
 from sklearn.linear_model import LogisticRegression,LogisticRegressionCV
 from sklearn.datasets import make_classification
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold
+
 
 def sigmoid(t):
     """Sigmoid function"""
@@ -33,7 +35,7 @@ class D_LogisticRegression():
     Currently we only allow for missingness pattern where each patients in a subsequent phase are only a subset of previous phases
     """
     
-    def __init__(self,alpha = [0.0],max_iter: int = 100, phases:dict=None):
+    def __init__(self,alphas = [1.0],max_iter: int = 100, phases:dict=None, cv: int = 3):
         """
         params
         -----------
@@ -49,7 +51,8 @@ class D_LogisticRegression():
         self.max_iter = max_iter
         self.phases = phases
         self.weights = 0
-        self.alpha = alpha
+        self.alphas = alphas
+        self.cv = cv
         
     def fit(self,X,y): 
         """
@@ -57,46 +60,79 @@ class D_LogisticRegression():
         """
         n_features = X.shape[1]
         y = 2*y - 1 #coordinate descent requires y to be within 1,-1
-        w = np.zeros(n_features + 1)
-        fit_phases = {}
+        #w = np.zeros(n_features + 1)
+        adjusted_phases = {}
         for phase, phase_features in self.phases.items():
-            fit_phases[phase + 1] = [feature + 1 for feature in phase_features]
+            adjusted_phases[phase + 1] = [feature + 1 for feature in phase_features]
         
         X = np.hstack((np.ones((len(X), 1)), X))
-        
-        for phase, phase_features in fit_phases.items():
+        w_phase = 0
+        for phase, phase_features in adjusted_phases.items():
             X_phase = X[:, phase_features]
             y_phase = y                    
             non_na_indices = np.where(np.sum(np.isnan(X_phase), axis=1) == 0)[0] #extract indices 
             X_phase = X_phase[non_na_indices, :]
             y_phase = y_phase[non_na_indices]
             X_phase = np.hstack((np.ones((len(X_phase), 1)), X_phase))
-            
-            if phase == 1: # if current phase is 1st phase
-                w_phase = np.zeros(len(phase_features) + 1)
-                new_features = phase_features
-            else: # if current phase is 1st phase
-                w_new_phase = np.zeros(len(phase_features) + 1)
-                for k in range(len(w_phase)):
-                    w_new_phase[k] = w_phase[k]
-                w_phase = w_new_phase
-                new_features = set(phase_features).difference(fit_phases[phase - 1])
-                new_features = list(new_features)
+            opt_alpha = self.get_cv_phase_alpha(X_phase,y_phase,w_phase,phase,phase_features,adjusted_phases)
+            w_phase = self.fit_phase(phase,phase_features,adjusted_phases,X_phase,y_phase,opt_alpha,w_phase)
+        
+        self.weights = w_phase
+            #w_phase_alphas = []
+            #for alpha in self.alphas:   
+                #def choose alpha here 
+            #    w_phase_alphas.append(self.fit_phase(phase,phase_features,adjusted_phases,X_phase,y_phase,alpha,w_phase))
+            # refit with chosen alpha
             
             
+    def fit_phase(self,phase,phase_features,adjusted_phases,X_phase,y_phase,alpha,w_phase):
+        #w_phase = self.weights
+        if phase == 1: # if current phase is 1st phase
+            w_phase = np.zeros(len(phase_features) + 1)
+            new_features = phase_features
+        else: # if current phase is 1st phase
+            w_new_phase = np.zeros(len(phase_features) + 1)
+            for k in range(len(w_phase)):
+                w_new_phase[k] = w_phase[k]
+            w_phase = w_new_phase
+            new_features = set(phase_features).difference(adjusted_phases[phase - 1])
+            new_features = list(new_features)
+        w_phase = self.coordinate_descent(X_phase,y_phase,w_phase,new_features,phase,alpha)
+        
+        return w_phase
+        #self.weights = w_phase
+        
+            
+    def coordinate_descent(self,X_phase,y_phase,w_phase,new_features,phase,alpha):
             X_phase_w_phase = X_phase.dot(w_phase)
             num_phase_samples = len(X_phase)
+            new_features.append(0) # adding bias term
             for t in range(self.max_iter):
-                new_features.append(0) # adding bias term
                 for j in new_features:
                     lips_const = sum([X_phase[i,j]**2 * np.exp(X_phase_w_phase[i]) * sigmoid(-X_phase_w_phase[i])**2 for i in range(num_phase_samples)]) 
                     old_w_phase_j = w_phase[j]
                     grad_j = -sum([y_phase[i] * X_phase[i,j] * sigmoid(-y_phase[i]*X_phase_w_phase[i]) for i in range(num_phase_samples)])
-                    w_phase[j] = np.sign(w_phase[j] - grad_j/lips_const) * soft_thresh(w_phase[j] - grad_j/lips_const, self.alpha[phase-1]/lips_const)
+                    w_phase[j] = np.sign(w_phase[j] - grad_j/lips_const) * soft_thresh(w_phase[j] - grad_j/lips_const, alpha/lips_const)
                     if old_w_phase_j != w_phase[j]:
                          X_phase_w_phase += w_phase[j] * X_phase[:,j] - old_w_phase_j * X_phase[:,j]
+            return w_phase
+    
+    def get_cv_phase_alpha(self,X_phase,y_phase,w_phase,phase,phase_features,adjusted_phases):
+        scores = np.zeros((self.cv,len(self.alphas)))
+        kf = KFold(n_splits=self.cv, random_state=None)
+        for i, (train_index , test_index) in enumerate(kf.split(X_phase)):
+            X_train , X_test = X_phase[train_index,:],X_phase[test_index,:]
+            y_train , y_test = y_phase[train_index] , y_phase[test_index]
+            for j,alpha in enumerate(self.alphas):
+                w_alpha_phase = self.fit_phase(phase,deepcopy(phase_features),deepcopy(adjusted_phases),X_train,y_train,alpha,w_phase)
+                y_alpha_preds =  sigmoid(np.matmul(X_test,w_alpha_phase))
+                scores[i,j] = roc_auc_score(y_test,y_alpha_preds)
+        av_scores = scores.mean(axis = 1)
+        av_scores = np.array(av_scores)
+        opt_alpha = self.alphas[np.argmax(av_scores)]
+        return opt_alpha
+
                         
-        self.weights = w_phase
         
     def predict_proba(self, X):
         """Predict probability for classifiers:
@@ -125,38 +161,36 @@ class D_LogisticRegression():
             preds[non_na_indices] = sigmoid(np.matmul(X_phase,w_phase))
         
         return np.vstack((1 - preds, preds)).transpose()
-        
-        
     
-    #def predict_proba(self,X,y):
-        
+            
+            
     
     
 if __name__ == '__main__':
 
-    X,y = make_classification(n_samples=400, n_features=5, n_informative=2,n_redundant = 0,random_state = 0)
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2)
+    X,y = make_classification(n_samples=350, n_features=5, n_informative=2,n_redundant = 0,random_state = 0)
+    #X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2)
     
     phases = {0: [0, 1, 2], 1: [0, 1, 2, 3, 4]}
-    #X[0:int(0.6 * X.shape[0]), [p for p in phases[1] if p not in phases[0]]] = np.nan
+    X[0:int(0.6 * X.shape[0]), [p for p in phases[1] if p not in phases[0]]] = np.nan
     
-    d_logistic_regression = D_LogisticRegression(alpha = [1.0,1.0],max_iter = 100, phases = phases)
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2)
+    d_logistic_regression = D_LogisticRegression(alphas = [0.5,1.0,1.5],max_iter = 100, phases = phases)
     d_logistic_regression.fit(X_train,y_train)
     
     print("Dynamic Logistic Regression - Weights:", d_logistic_regression.weights[1:])
     print("Dynamic Logistic Regression - Bias:", d_logistic_regression.weights[0])
     
     
-    sklearn_logreg = LogisticRegression(penalty = 'l1', C = 1.0,solver = 'liblinear',max_iter = 100)
-    sklearn_logreg.fit(X_train,y_train)
+    #sklearn_logreg = LogisticRegression(penalty = 'l1', C = 1.0,solver = 'liblinear',max_iter = 100)
+    #sklearn_logreg.fit(X_train,y_train)
     
-    print("Scikit-learn - Weights:", sklearn_logreg.coef_)
-    print("Scikit-learn - Bias:", sklearn_logreg.intercept_)
+    #print("Scikit-learn - Weights:", sklearn_logreg.coef_)
+    #print("Scikit-learn - Bias:", sklearn_logreg.intercept_)
     
     
     print("Dynamic Logistic Regression - AUROC:",roc_auc_score(y_test,d_logistic_regression.predict_proba(X_test)[:,1]))
-    print("Scikit-learn - AUROC:",roc_auc_score(y_test,d_logistic_regression.predict_proba(X_test)[:,1]))
+    #print("Scikit-learn - AUROC:",roc_auc_score(y_test,sklearn_logreg.predict_proba(X_test)[:,1]))
 
     
-    #print("Dynamic Logistic Regression - AUROC:",d_logistic_regression.predict_proba(X_test))
    
